49bd40c80170644940b6fadbfa2e3089865a9724
silpaps <49475828+silpaps@users.noreply.github.com>
2024-12-01 12:17:45 +0530

    Set1_problm1
    Prob 1 : Write a program that takes an integer, then a string, then a char from the user and prints them in the screen.
    
    
    Input:  2 Name y
    
    Expected Output:
    
    2
    
    Name
    
    y

---
+++ Inputone.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Inputone {
+    public static void main(String[] args) {
+    
+    Scanner scan = new Scanner(System.in);
+    int num = scan.nextInt();
+    String a = scan.next();
+    String b = scan.next();
+    System.out.println(num);
+    System.out.println(a);
+    System.out.println(b);
+    
+    }
+}
