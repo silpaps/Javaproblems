8b3c1e368ce9bb7ffe418e92a55eafa8c1b76baf
silpaps <49475828+silpaps@users.noreply.github.com>
2024-12-06 19:59:48 +0530

    Pattern8
    Write a Program that takes input n and prints the below pattern and upload the code to your github repo.
    
    
    
    Sample 1:
    
    Input:
    
    n = 4
    
    Expected output:
    
    *
    
    **
    
    ***
    
    ****
    
    
    
    Sample 2
    
    Input:
    
    n= 6
    
    *
    
    **
    
    ***
    
    ****
    
    *****
    
    ******

---
+++ Pattern8.java
@@ -0,0 +1,11 @@
+class Pattern8 {
+  public static void main(String[] args) {
+      int n=4;
+      for (int row=1; row<=n ; row++){
+        for (int col=1;col<=row ;col++){
+          System.out.print("*");
+        }
+          System.out.println();
+      }
+  }
+}
